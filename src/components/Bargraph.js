6b6c175fc70a3d4eef0c3ca05912ac5ae0f7d4d3
harsha-9010 <harshavardhanpeela055@gmail.com>
2021-12-26 23:00:50 +0530

    Update on 26/12

---
+++ src/components/Bargraph.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Bargraph = () => {
+  return;
+  <Chart
+    width={"500px"}
+    height={"300px"}
+    chartType="Bar"
+    loader={<div>Loading Chart</div>}
+    data={[
+      ["Year", "Sales", "Expenses", "Profit"],
+      ["2014", 1000, 400, 200],
+      ["2015", 1170, 460, 250],
+      ["2016", 660, 1120, 300],
+      ["2017", 1030, 540, 350],
+    ]}
+    options={{
+      // Material design options
+      chart: {
+        title: "Company Performance",
+        subtitle: "Sales, Expenses, and Profit: 2014-2017",
+      },
+    }}
+    // For tests
+    rootProps={{ "data-testid": "2" }}
+  />;
+};
+export default Bargraph;
